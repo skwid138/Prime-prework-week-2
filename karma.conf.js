a83fc6810d344c2b375cdedc08d5be5d1bf73506
Millie Walsh <millie@primeacademy.io>
2017-02-14 11:10:17 -0600

    french-inquisition

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'solution/*.js',
+      'french-inquisition/*.js',
       // 'assignment/*.js',
       'tests/*.js'
     ],
