f5be75f47c2bd5bf1949d1bffbcf6027c1610a65
Millie Walsh <millie@primeacademy.io>
2017-03-16 11:11:54 -0500

    remove merge conflict and add answers

--- assignment/scope.js
+++ assignment/scope.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * OPTIONAL Pro Mode Assignment
  *
@@ -16,37 +15,38 @@
  * the way they are. If you got them right, then you should order up some
  * pizza and celebrate your understanding of these difficult topics!
  */
-    var theNumber = 1;
 
-    var michelangelo = function (value) {
-        value = (value * 2);
-        return value;
-    };
+var theNumber = 1;
 
-    var donatello = function (value, anotherValue) {
-        value = anotherValue + value;
-        anotherValue = value * anotherValue;
-        return anotherValue;
-    };
+var michelangelo = function (value) {
+    value = (value * 2);
+    return value;
+};
 
-    var leonardo = function (value) {
-        var anotherValue = value;
-        value *= 4;
-        theNumber = value / 2;
-        return (value + anotherValue);
-    };
+var donatello = function (value, anotherValue) {
+    value = anotherValue + value;
+    anotherValue = value * anotherValue;
+    return anotherValue;
+};
 
-    var raphael = function (value, anotherValue, yetAnotherValue) {
-        value = value * anotherValue + yetAnotherValue;
-        return yetAnotherValue;
-    };
+var leonardo = function (value) {
+    var anotherValue = value;
+    value *= 4;
+    theNumber = value / 2;
+    return (value + anotherValue);
+};
 
-    // console.log(michelangelo(theNumber) + leonardo(theNumber));
-    // ^ Before running the script, what number would you expect the line above to evaluate to?
-    var resultOne = undefined; // replace undefined with the result
+var raphael = function (value, anotherValue, yetAnotherValue) {
+    value = value * anotherValue + yetAnotherValue;
+    return yetAnotherValue;
+};
 
-    theNumber = theNumber * 2;
+// console.log(michelangelo(theNumber) + leonardo(theNumber));
+// ^ Before running the script, what number would you expect the line above to evaluate to?
+var resultOne = 7; // replace undefined with the result
 
-    // console.log((donatello(theNumber, 2)) - (raphael(3, 2, theNumber)));
-    // ^ Before running the script, what number would you expect the line above to evaluate to?
-    var resultTwo = undefined; // replace undefined with the result
+theNumber = theNumber * 2;
+
+// console.log((donatello(theNumber, 2)) - (raphael(3, 2, theNumber)));
+// ^ Before running the script, what number would you expect the line above to evaluate to?
+var resultTwo = 8; // replace undefined with the result
